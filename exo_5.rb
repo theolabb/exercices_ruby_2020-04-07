eaeda0c39298316747b5d21b38910ef728e6c1be
theo <theolabb@gmail.com>
2020-04-07 23:43:42 +0200

    je pense que #{} permet de mettre le calcul en commentaire et de n'afficher que le résultat

---
+++ exo_5.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" # affiche ce qu'il y a entre les quotes.
+puts "Travail : #{10 * 5 * 11}" # affiche ce qu'il y a entre les quotes et calcul le 10*5*11*60 pour afficher juste le résultat.
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"# affiche ce qu'il y a entre les quotes et calcul le 10*5*11*60 pour afficher juste le résultat.
+
+puts "Et en secondes ?"# affiche ce qu'il y a entre les quotes.
+
+puts 10 * 5 * 11 * 60 * 60 # callul le 10*5*11*60*60  pour afficher juste le résultat. 
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affiche ce qu'il y a entre les quotes
+
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"#affiche ce qu'il y a entre les quotes
+
+puts "C'est drôle ça, faisons-en plus :"#affiche ce qu'il y a entre les quote
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"

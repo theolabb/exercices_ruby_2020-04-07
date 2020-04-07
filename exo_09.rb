5f04222788f388981245d650868b1b95699cbeb9
theo <theolabb@gmail.com>
2020-04-08 00:57:54 +0200

    prg bonjour prénom nom

---
+++ exo_09.rb
@@ -0,0 +1,11 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> " 
+prénom = gets.chomp
+puts "Et ton nom de famille ?"
+print "> "
+nom_de_famille = gets.chomp
+print "Bonjour, "
+print prénom
+print " "
+print nom_de_famille
+puts "!"
